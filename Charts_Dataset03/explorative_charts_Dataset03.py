932504d129e0ac16ea20a86987acab391fe43029
vvannoesel <V.S.vanNoesel@student.tudelft.nl>
2026-05-22 14:06:04 +0200

    generated figures for the presentation

--- Charts_Dataset03/explorative_charts_Dataset03.py
+++ Charts_Dataset03/explorative_charts_Dataset03.py
@@ -6692,3 +6692,101 @@ plt.grid()
 
 # # plt.tight_layout()
 # # plt.show()
+
+
+
+
+
+
+
+
+# ---------- plot for presentation -----------------------
+
+
+
+# Create 2x4 subplot grid (8 total)
+fig, axes = plt.subplots(1, 3, figsize=(22, 8))
+axes = axes.flatten()
+
+# Set 1 common x-y label for the entire figure
+fig.supxlabel('Mean Completion Score (%)', fontsize = 12)
+fig.supylabel('Keyword Presence (%)', fontsize = 12)
+
+
+plot_keyword(axes[0], small_dataset_algorithm, "Algorithm",  "Presence (%)")
+plot_keyword(axes[1], small_dataset_heuristic, "Heuristics", "Presence (%)")
+# plot_keyword(axes[2], small_dataset_false_confidence, "Declared Confidence", "Presence (%)")
+
+
+# Make a legend
+# specify colors
+colors = ['tab:blue', 'tab:orange', 'tab:red', 'tab:green', 'tab:purple', 'tab:brown']
+
+# --- Design Legend Group Markers ---
+spacer_handle = (
+Line2D([], [], marker='o', color='none', markerfacecolor='none', markersize=10)
+)
+line_handle = (
+Line2D([], [], marker='o', color='grey', linestyle = 'none'),
+),
+occupancy_handle = (
+Line2D([], [], marker='v', color='grey', linestyle = 'none'),
+)
+
+line_handle_6 = (
+Line2D([], [], marker='o', color='grey', linestyle = 'none'),
+),
+occupancy_handle_6 = (
+Line2D([], [], marker='v', color='grey', linestyle = 'none'),
+)
+
+line_handle_15 = (
+Line2D([], [], marker='s', color='grey', linestyle = 'none'),
+),
+occupancy_handle_15 = (
+Line2D([], [], marker='d', color='grey', linestyle = 'none'),
+)
+
+
+# Create legend order with spacers for grouping
+handles = [
+spacer_handle,
+line_handle,
+occupancy_handle,
+spacer_handle,
+
+Line2D([], [], marker='s', color=colors[0], linestyle='None', markersize = 10),  # Adjacency JSON
+Line2D([], [], marker='s', color=colors[1], linestyle='None', markersize = 10),  # Adjacency Text
+Line2D([], [], marker='s', color=colors[3], linestyle='None', markersize = 10),  # JSON
+Line2D([], [], marker='s', color=colors[2], linestyle='None', markersize = 10),  # JPG
+Line2D([], [], marker='s', color=colors[4], linestyle='None', markersize = 10),  # Tokenized
+Line2D([], [], marker='s', color=colors[5], linestyle='None', markersize = 10),  # ASCII
+
+]
+
+# Add labels to the markers
+labels = [
+r"$\bf{Wall\ Encodings}$",
+"Line-wall", "Occupancy grid",
+r"$\bf{Maze\ Representations}$",
+"Adjacency List JSON", "Adjacency List Text", "JPG", "JSON", "Tagged per-cell", "ASCII"
+]
+
+axes[2].axis("off")
+
+axes[2].legend(
+    handles,
+    labels,
+    handler_map={tuple: HandlerTuple(ndivide=None)},
+    loc="center",
+    title="Legend",
+    frameon=True
+)
+
+
+plt.suptitle(r'$\bf{Keyword\ Presence\ as\ a\ Function\ of\ Mean\ Completion\ Score}$'
+                   '\nGemini 2.5 Pro, All Output Frames of Reference, Maze Sizes 6x6/13x13 and 15x15/31x31',
+                    fontsize=14)
+
+
+plt.show()
